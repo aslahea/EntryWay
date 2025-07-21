7e841fd79f0acfa96fcfffc1675964cd9ff49348
Ayoobkibrahim <ayoobkibrahim01.com>
2025-07-21 17:41:01 +0530

    Forms created

--- Web_Base/web_app/models.py
+++ Web_Base/web_app/models.py
@@ -1,23 +1,19 @@
-from django.contrib.auth.models import AbstractUser 
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 GENDER_CHOICES = [
-    ('Male','Male'),
-    ('Female','Female'),
-    ('Other','Other')
+    ('Male', 'Male'),
+    ('Female', 'Female'),
+    ('Other', 'Other')
 ]
 
-class CustomUser(AbstractUser) :
+
+class CustomUser(AbstractUser):
     full_name = models.CharField(max_length=100)
     phone = models.CharField(max_length=15)
-    gender = models.CharField(max_length=10,choices=GENDER_CHOICES)
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
     date_of_birth = models.DateField()
     is_deleted = models.BooleanField(default=False)
 
-    def __str__ (self):
-        return str(self.username or "")    
-    
-
-   
-
-
+    def __str__(self):
+        return str(self.username or "")
