28a58e7c147431e872ca0bde528beef503c276ba
Ayoobkibrahim <ayoobkibrahim01.com>
2025-07-21 20:23:40 +0530

    Unused import remove

--- Web_Base/web_app/admin.py
+++ Web_Base/web_app/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import CustomUser
 from django.contrib.auth.admin import UserAdmin
-from django.contrib.admin import SimpleListFilter
 
 
 @admin.register(CustomUser)
